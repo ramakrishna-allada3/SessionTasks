1f3047f957a6c165772de9c0b7b2a68d61ce9b50
ramakrishna-allada3 <ramakrishna.allada3@gmail.com>
2021-11-18 11:01:20 +0530

    Added one more task

--- 18Nov/scripts.js
+++ 18Nov/scripts.js
@@ -1,8 +1,24 @@
-function changeColour() {
-    var x = Math. floor(Math. random() * 256);
-    var y = Math. floor(Math. random() * 256);
-    var z = Math. floor(Math. random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    console. log(bgColor);
-    document.body.style.background = bgColor;
+
+var a = 0;
+var b = 0;
+
+window.onload = (event) => {
+    var num1 = document.getElementById("number1");
+    var num2 = document.getElementById("number2");
+    a = Math.floor(Math.random()*10);
+    b = Math.floor(Math.random()*10);
+    num1.innerText = a;
+    num2.innerText = b;
+};
+
+function checkAnswer() {
+    var answer = document.getElementById("answer").value;
+    if (a+b === parseInt(answer)) {
+        alert("Answer is correct");
+        location.reload(); 
+    }
+    else {
+        alert("Your answer is incorrect. Please check and try again");
+    }
+
 }
